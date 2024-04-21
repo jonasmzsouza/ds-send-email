7001c803edaa2820810756b6a706a75f6e0795a8
Jonas Souza <jonasmzsouza@gmail.com>
2024-04-21 15:35:14 -0300

    add emailservice

--- src/main/java/com/devsuperior/sendemail/resources/EmailResource.java
+++ src/main/java/com/devsuperior/sendemail/resources/EmailResource.java
@@ -1,6 +1,8 @@
 package com.devsuperior.sendemail.resources;
 
 import com.devsuperior.sendemail.dto.EmailDTO;
+import com.devsuperior.sendemail.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/emails")
 public class EmailResource {
 
+    @Autowired
+    private EmailService service;
+
     @PostMapping
     public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+        service.sendEmail(dto);
         return ResponseEntity.noContent().build();
     }
 }
