0f4d361fce4effcfaa0ca13cc1eeed910f54e77a
Jonas Souza <jonasmzsouza@gmail.com>
2024-04-21 14:46:22 -0300

    project created, emaildto

---
+++ src/main/java/com/devsuperior/sendemail/resources/EmailResource.java
@@ -0,0 +1,18 @@
+package com.devsuperior.sendemail.resources;
+
+import com.devsuperior.sendemail.dto.EmailDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/emails")
+public class EmailResource {
+
+    @PostMapping
+    public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+        return ResponseEntity.noContent().build();
+    }
+}
